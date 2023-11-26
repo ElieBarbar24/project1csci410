01783e5cd3e9d1a4fa7f103c0f3796e0479fb029
JenniferRaad <raadjenny11@gmail.com>
2023-11-26 01:55:21 -0800

    TimeConverter

---
+++ lib/UnitConverter/TimeConverter.dart
@@ -0,0 +1,24 @@
+class TimeConverter{
+  double value;
+  Map<String, double> conversionRates = {
+    'ns': 1,
+    'µs': 1000,
+    'ms': 1000000,
+    's': 1000000000,
+    'min': 60000000000,
+    'h': 3600000000000,
+    'd': 86400000000000,
+    'wk': 604800000000000
+  };
+  TimeConverter(this.value);
+
+  double convert(String from, String to) {
+    if(conversionRates.containsKey(from) && conversionRates.containsKey(to)) {
+      double fromRate = conversionRates[from]!;
+      double toRate = conversionRates[to]!;
+      return (value * fromRate) / toRate;
+    }else{
+      throw Exception('Invalid units');
+    }
+  }
+}
