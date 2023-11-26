aa82c1fce8f5e492f6f851f64e08117f4184dc87
ElieBarbar24 <elie24barbar@gmail.com>
2023-11-26 14:27:23 +0200

    correcting some mistakes and creating the calculator table

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:project1csci410/UnitConverter/AreaConverter.dart';
+import 'package:project1csci410/UnitConverter/DataConverter.dart';
+import 'package:project1csci410/UnitConverter/FrequencyConverter.dart';
+import 'package:project1csci410/UnitConverter/LengthConverter.dart';
+import 'package:project1csci410/UnitConverter/SpeedConverter.dart';
+import 'package:project1csci410/UnitConverter/TemperatureConverter.dart';
+import 'package:project1csci410/UnitConverter/TimeConverter.dart';
+import 'package:project1csci410/UnitConverter/VolumeConverter.dart';
+import 'package:project1csci410/UnitConverter/WeightConverter.dart';
 
 class Home extends StatefulWidget {
   Home(
@@ -23,33 +32,77 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late String categorie;
+  late List<String> type;
+  late Map<String, dynamic> units;
+  late List<String> from;
+  late Map<String, String> fromto;
+  late List<String> to;
+  late String vfrom;
+  late String vto;
+  int listIndex = 0;
+
+  @override
+  void initState() {
+    type = widget.types;
+    units = widget.quantities;
+    from = widget.from;
+    fromto = widget.fromto;
+    to = widget.from;
+    vfrom = widget.vfrom;
+    vto = widget.vto;
+    super.initState();
+  }
+
+  String valuein = '0';
+
+  calculate() {
+    if (widget.categorie.compareTo('Material') == 0) {
+      if (listIndex == 0) {
+        double res = double.parse(valuein);
+        double answer = LengthConverter(res).convert(vfrom!, vto!);
+        valueout = answer.toString();
+      } else if (listIndex == 1) {
+        double res = double.parse(valuein);
+        double answer = AreaConverter(res).convert(vfrom!, vto!);
+        valueout = answer.toString();
+      } else if (listIndex == 2) {
+        double res = double.parse(valuein);
+        double answer = VolumeConverter(res).convert(vfrom!, vto!);
+        valueout = answer.toString();
+      } else if (listIndex == 3) {
+        double res = double.parse(valuein);
+        double answer = WeightConverter(res).convert(vfrom!, vto!);
+        valueout = answer.toString();
+      } else if (listIndex == 4) {
+        double res = double.parse(valuein);
+        double answer = TemperatureConverter(res).convert(vfrom!, vto!);
+        valueout = answer.toString();
+      }
+    } else {
+      if (listIndex == 0) {
+        double res = double.parse(valuein);
+        double answer = SpeedConverter(res).convert(vfrom!, vto!);
+        valueout = answer.toString();
+      } else if (listIndex == 1) {
+        double res = double.parse(valuein);
+        double answer = DataConverter(res).convert(vfrom!, vto!);
+        valueout = answer.toString();
+      } else if (listIndex == 2) {
+        double res = double.parse(valuein);
+        double answer = TimeConverter(res).convert(vfrom!, vto!);
+        valueout = answer.toString();
+      } else if (listIndex == 3) {
+        double res = double.parse(valuein);
+        double answer = FrequencyConverter(res).convert(vfrom!, vto!);
+        valueout = answer.toString();
+      }
+    }
+  }
+
+  String valueout = '0';
   @override
   Widget build(BuildContext context) {
-    late String categorie;
-    late List<String> type;
-    late Map<String, dynamic> units;
-    late List<String> from;
-    late Map<String, String> fromto;
-    late List<String> to;
-    late String vfrom;
-    late String vto;
-    int listIndex = 0;
-
-    @override
-    void initState() {
-      type = widget.types;
-      units = widget.quantities;
-      from = widget.from;
-      fromto = widget.fromto;
-      to = widget.from;
-      vfrom = widget.vfrom;
-      vto = widget.vto;
-      super.initState();
-    }
-
-    String valuein = '0';
-
-    String valueout = '0';
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -180,7 +233,6 @@ class _HomeState extends State<Home> {
                 onChanged: (String? selected) {
                   setState(() {
                     vto = selected!;
-
                   });
                 },
                 items: to.map((String value) {
@@ -199,6 +251,304 @@ class _HomeState extends State<Home> {
           const SizedBox(
             height: 100,
           ),
+          Table(
+            defaultColumnWidth: const FractionColumnWidth(.23),
+            children: [
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: RawMaterialButton(
+                    onPressed: () {
+                      setState(() {
+                        if (valuein.compareTo('0') == 0) {
+                          valuein = '7';
+                          calculate();
+                        } else {
+                          valuein = "${valuein}7";
+                          calculate();
+                        }
+                      });
+                    },
+                    fillColor: Colors.black,
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(16.0),
+                    child: const Text(
+                      '7',
+                      style: TextStyle(color: Color(0xFFBDBDBD), fontSize: 30),
+                    ),
+                  ),
+                ),
+                RawMaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      if (valuein.compareTo('0') == 0) {
+                        valuein = '8';
+                        calculate();
+                      } else {
+                        valuein = "${valuein}8";
+                        calculate();
+                      }
+                    });
+                  },
+                  fillColor: Colors.black,
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(16.0),
+                  child: const Text(
+                    '8',
+                    style: TextStyle(color: Color(0xFFBDBDBD), fontSize: 30),
+                  ),
+                ),
+                RawMaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      if (valuein.compareTo('0') == 0) {
+                        valuein = '9';
+                        calculate();
+                      } else {
+                        valuein = "${valuein}9";
+                        calculate();
+                      }
+                    });
+                  },
+                  fillColor: Colors.black,
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(16.0),
+                  child: const Text(
+                    '9',
+                    style: TextStyle(color: Color(0xFFBDBDBD), fontSize: 30),
+                  ),
+                ),
+                RawMaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      if (valuein.length == 1) {
+                        valuein = '0';
+                        calculate();
+                        return;
+                      } else {
+                        valuein = valuein.substring(0, valuein.length - 1);
+                        calculate();
+                      }
+                    });
+                  },
+                  fillColor: Colors.black,
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(16.0),
+                  child: const Icon(
+                    Icons.backspace_outlined,
+                    size: 35,
+                    color: Color(0xFFBDBDBD),
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: RawMaterialButton(
+                    onPressed: () {
+                      setState(() {
+                        if (valuein.compareTo('0') == 0) {
+                          valuein = '4';
+                          calculate();
+                        } else {
+                          valuein = "${valuein}4";
+                          calculate();
+                        }
+                      });
+                    },
+                    fillColor: Colors.black,
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(16.0),
+                    child: const Text(
+                      '4',
+                      style: TextStyle(color: Color(0xFFBDBDBD), fontSize: 30),
+                    ),
+                  ),
+                ),
+                RawMaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      if (valuein.compareTo('0') == 0) {
+                        valuein = '5';
+                        calculate();
+                      } else {
+                        valuein = "${valuein}5";
+                        calculate();
+                      }
+                    });
+                  },
+                  fillColor: Colors.black,
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(16.0),
+                  child: const Text(
+                    '5',
+                    style: TextStyle(color: Color(0xFFBDBDBD), fontSize: 30),
+                  ),
+                ),
+                RawMaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      if (valuein.compareTo('0') == 0) {
+                        valuein = '6';
+                        calculate();
+                      } else {
+                        valuein = "${valuein}6";
+                        calculate();
+                      }
+                    });
+                  },
+                  fillColor: Colors.black,
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(16.0),
+                  child: const Text(
+                    '6',
+                    style: TextStyle(color: Color(0xFFBDBDBD), fontSize: 30),
+                  ),
+                ),
+                RawMaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      valuein = "0";
+                      calculate();
+                    });
+                  },
+                  fillColor: Colors.black,
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'C',
+                    style: TextStyle(color: Colors.red[500], fontSize: 30),
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: RawMaterialButton(
+                    onPressed: () {
+                      setState(() {
+                        if (valuein.compareTo('0') == 0) {
+                          valuein = '1';
+                          calculate();
+                        } else {
+                          valuein = "${valuein}1";
+                          calculate();
+                        }
+                      });
+                    },
+                    fillColor: Colors.black,
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(16.0),
+                    child: const Text(
+                      '1',
+                      style: TextStyle(color: Color(0xFFBDBDBD), fontSize: 30),
+                    ),
+                  ),
+                ),
+                RawMaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      if (valuein.compareTo('0') == 0) {
+                        valuein = '2';
+                        calculate();
+                      } else {
+                        valuein = "${valuein}2";
+                        calculate();
+                      }
+                    });
+                  },
+                  fillColor: Colors.black,
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(16.0),
+                  child: const Text(
+                    '2',
+                    style: TextStyle(color: Color(0xFFBDBDBD), fontSize: 30),
+                  ),
+                ),
+                RawMaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      if (valuein.compareTo('0') == 0) {
+                        valuein = '3';
+                        calculate();
+                      } else {
+                        valuein = "${valuein}3";
+                        calculate();
+                      }
+                    });
+                  },
+                  fillColor: Colors.black,
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(16.0),
+                  child: const Text(
+                    '3',
+                    style: TextStyle(color: Color(0xFFBDBDBD), fontSize: 30),
+                  ),
+                ),
+                RawMaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      String temp;
+                      temp = vfrom!;
+                      vfrom = vto;
+                      vto = temp;
+                      calculate();
+                    });
+                  },
+                  fillColor: Colors.black,
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(21.0),
+                  child: const Icon(
+                    Icons.multiple_stop,
+                    size: 25,
+                    color: Color(0xFFBDBDBD),
+                  ),
+                ),
+              ]),
+              TableRow(children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: RawMaterialButton(
+                    onPressed: () {
+                      if (valuein.compareTo('0') == 0) return;
+                      setState(() {
+                        valuein = "${valuein}0";
+                        calculate();
+                      });
+                    },
+                    fillColor: Colors.black,
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(16.0),
+                    child: const Text(
+                      '0',
+                      style: TextStyle(color: Color(0xFFBDBDBD), fontSize: 30),
+                    ),
+                  ),
+                ),
+                RawMaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      valuein = "$valuein.";
+                      calculate();
+                    });
+                  },
+                  fillColor: Colors.black,
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(16.0),
+                  child: const Text(
+                    '.',
+                    style: TextStyle(color: Color(0xFFBDBDBD), fontSize: 30),
+                  ),
+                ),
+                RawMaterialButton(
+                  onPressed: () {},
+                ),
+                RawMaterialButton(
+                  onPressed: () {},
+                ),
+              ])
+            ],
+          )
         ],
       ),
     );
