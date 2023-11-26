db5960a26a092eb5842089c2283c737b7c061c5e
JenniferRaad <raadjenny11@gmail.com>
2023-11-26 01:48:36 -0800

    DataConverter

---
+++ lib/UnitConverter/DataConverter.dart
@@ -0,0 +1,24 @@
+class DataConverter{
+  double value;
+  Map<String, double> conversionRates = {
+    'b': 1,
+    'B': 8,
+    'kb': 1000,
+    'kB': 8000,
+    'Mb': 1000000,
+    'MB': 8000000,
+    'Gb': 1000000000,
+    'GB': 8000000000
+  };
+  DataConverter(this.value);
+
+  double convert(String from,String to) {
+    if(conversionRates.containsKey(from) && conversionRates.containsKey(to)) {
+      double fromRate = conversionRates[from]!;
+      double toRate = conversionRates[to]!;
+      return(value * fromRate) / toRate;
+    }else{
+      throw Exception('Invalid units');
+    }
+  }
+}
